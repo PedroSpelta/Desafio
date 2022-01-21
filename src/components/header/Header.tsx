a5528a6487196989924bc512b92692c6457eaf51
PedroSpelta <p.h.s.8989@hotmail.com>
2022-01-20 22:15:58 -0300

    adding contact me

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,12 +3,21 @@ import Logo from "./Logo";
 
 function Header() {
   return (
+    <div>
+      <div className="bg-gray-300 text-sm text-gray-500 flex justify-center">
+        <div className=" w-full max-w-3xl flex justify-end">
+          <p className="cursor-pointer">
+          Contact me
+          </p>
+        </div>
+      </div>
     <div className="flex w-full justify-center border-b border-[#5a9de0]">
-      <div className="w-full p-2  max-w-3xl ">
+      <div className="w-full p-2 max-w-3xl ">
         <p className="pl-10">
           <Logo />
         </p>
       </div>
+    </div>
     </div>
   );
 }
