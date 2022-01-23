faf356d46d4aef5300e362e0849b8b8e8afbb71e
PedroSpelta <p.h.s.8989@hotmail.com>
2022-01-23 05:32:06 -0300

    adding paint form

---
+++ src/components/main/PaintCalculator.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function PaintCalculator() {
+  return <div className="flex flex-col justify-center items-center">
+    <div className="max-w-5xl bg-red-400 w-full h-80">tese</div>
+  </div>;
+}
+
+export default PaintCalculator;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import { useState } from 'react';
 import Header from '../components/header/Header';
+import PaintCalculator from '../components/main/PaintCalculator';
 
 const Home: NextPage = () => {
   const [language, setLanguage] = useState('english');
@@ -15,6 +16,7 @@ const Home: NextPage = () => {
 
       <main >
         <Header />
+        <PaintCalculator/>
         
       </main>
 
